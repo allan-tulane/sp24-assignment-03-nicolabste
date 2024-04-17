1897dcaebc0d7b4d93b65b3ed19b2247dcb820c8
nicolabste <126182691+nicolabste@users.noreply.github.com>
2024-04-17 16:41:40 +0000

    commit

--- main.py
+++ main.py
@@ -7,95 +7,72 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+    if S == "":
+        return len(T)
+    elif T == "":
+        return len(S)
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+        if S[0] == T[0]:
+            return MED(S[1:], T[1:])
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return 1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:]))
 
 
-def fast_MED(S, T, memo={}):
-  # Check for already computed result
-  if (S, T) in memo:
-      return memo[(S, T)]
 
-  # Base cases when either string is empty
-  if not S:
-      return len(T)
-  if not T:
-      return len(S)
+def fast_MED(S, T, MED={}):
+    if (S, T) in MED:
+        return MED[(S, T)]
+    if S == "":
+        return len(T)
+    elif T == "":
+        return len(S)
+    else:
+        if S[0] == T[0]:
+            result = fast_MED(S[1:], T[1:], MED)
+        else:
+            result = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED), fast_MED(S[1:], T[1:], MED))
+        MED[(S, T)] = result
+        return result
 
-  # Recursive cases: matching characters or handling edits
-  if S[0] == T[0]:
-      result = fast_MED(S[1:], T[1:], memo)
-  else:
-      result = 1 + min(
-          fast_MED(S, T[1:], memo),  # Insertion
-          fast_MED(S[1:], T, memo),  # Deletion
-          fast_MED(S[1:], T[1:], memo)  # Substitution
-      )
 
-  memo[(S, T)] = result
-  return result
+def fast_align_MED(S, T, MED={}):
+    if (S, T) in MED:
+        return MED[(S, T)]
 
-def fast_align_MED(S, T, memo={}):
-  # Check for already computed result
-  if (S, T) in memo:
-      return memo[(S, T)]
+    if S == "":
+        alignment_S = "-" * len(T)
+        alignment_T = T
+        return len(T), alignment_S, alignment_T
 
-  # Handling base cases when one string is empty
-  if not S:
-      alignment_S = "-" * len(T)
-      alignment_T = T
-      return len(T), alignment_S, alignment_T
-  if not T:
-      alignment_S = S
-      alignment_T = "-" * len(S)
-      return len(S), alignment_S, alignment_T
+    elif T == "":
+        alignment_S = S
+        alignment_T = "-" * len(S)
+        return len(S), alignment_S, alignment_T
 
-  # Handling recursive cases
-  insert_cost, insert_alignment_S, insert_alignment_T = fast_align_MED(S, T[1:], memo)
-  delete_cost, delete_alignment_S, delete_alignment_T = fast_align_MED(S[1:], T, memo)
-  replace_cost, replace_alignment_S, replace_alignment_T = fast_align_MED(S[1:], T[1:], memo)
+    else:
+        if S[0] == T[0]:
+            result, alignment_S, alignment_T = fast_align_MED(S[1:], T[1:], MED)
+            alignment_S = S[0] + alignment_S
+            alignment_T = T[0] + alignment_T
+        else:
+            insert_cost, insert_alignment_S, insert_alignment_T = fast_align_MED(S, T[1:], MED)
+            delete_cost, delete_alignment_S, delete_alignment_T = fast_align_MED(S[1:], T, MED)
+            replace_cost, replace_alignment_S, replace_alignment_T = fast_align_MED(S[1:], T[1:], MED)
 
-  # Determining minimum cost edit
-  if S[0] == T[0]:
-      direct_match_cost, direct_alignment_S, direct_alignment_T = fast_align_MED(S[1:], T[1:], memo)
-      direct_cost = direct_match_cost
-      if direct_cost <= min(insert_cost, delete_cost, replace_cost):
-          alignment_S = S[0] + direct_alignment_S
-          alignment_T = T[0] + direct_alignment_T
-          result = direct_cost
-      else:
-          result, alignment_S, alignment_T = determine_minimum(
-              insert_cost, insert_alignment_S, insert_alignment_T,
-              delete_cost, delete_alignment_S, delete_alignment_T,
-              replace_cost, replace_alignment_S, replace_alignment_T,
-              S[0], T[0]
-          )
-  else:
-      result, alignment_S, alignment_T = determine_minimum(
-          insert_cost, insert_alignment_S, insert_alignment_T,
-          delete_cost, delete_alignment_S, delete_alignment_T,
-          replace_cost, replace_alignment_S, replace_alignment_T,
-          S[0], T[0]
-      )
+            if insert_cost <= delete_cost and insert_cost <= replace_cost:
+                result = 1 + insert_cost
+                alignment_S = '-' + insert_alignment_S
+                alignment_T = T[0] + insert_alignment_T
+            elif delete_cost <= insert_cost and delete_cost <= replace_cost:
+                result = 1 + delete_cost
+                alignment_S = S[0] + delete_alignment_S
+                alignment_T = '-' + delete_alignment_T
+            else:
+                result = 1 + replace_cost
+                alignment_S = S[0] + replace_alignment_S
+                alignment_T = T[0] + replace_alignment_T
 
-  memo[(S, T)] = result, alignment_S, alignment_T
-  return result, alignment_S, alignment_T
+        MED[(S, T)] = result, alignment_S, alignment_T
+        return result, alignment_S, alignment_T
 
-def determine_minimum(insert_cost, insert_alignment_S, insert_alignment_T,
-                    delete_cost, delete_alignment_S, delete_alignment_T,
-                    replace_cost, replace_alignment_S, replace_alignment_T,
-                    char_S, char_T):
-  if insert_cost <= delete_cost and insert_cost <= replace_cost:
-      return 1 + insert_cost, '-' + insert_alignment_S, char_T + insert_alignment_T
-  elif delete_cost <= replace_cost:
-      return 1 + delete_cost, char_S + delete_alignment_S, '-' + delete_alignment_T
-  else:
-      return 1 + replace_cost, char_S + replace_alignment_S, char_T + replace_alignment_T
+
